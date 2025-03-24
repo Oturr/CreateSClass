9c565997b23f73f7edbeae1158a1bad38a64bbb0
Oturr <132491408+Oturr@users.noreply.github.com>
2025-03-24 21:03:51 +0300

    Add files via upload

---
+++ GenerateSClasses/GenerateSClasses/Program.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenerateSClasses
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			string outputPath = @"C:\Users\user\Desktop\0\GeneratedClasses";
+
+			bool exitState = true;
+			while (exitState)
+			{
+				Console.Write("Название класса: ");
+				string className = Console.ReadLine();
+				Console.Write("Название методов: ");
+				string methodName = Console.ReadLine();
+				Console.Write("dt: ");
+				string dtName = Console.ReadLine();
+				Console.Write("Ошибка при получении чего?: ");
+				string fErMsg = Console.ReadLine();
+				Console.Write("Уже есть что?: ");
+				string sErMsg = Console.ReadLine();
+				Console.Write("Ошибка при чего?: ");
+				string tErMsg = Console.ReadLine();
+
+				Console.Write("Таблица: ");
+				string bdTab = Console.ReadLine();
+				Console.Write("Индекс таблицы: ");
+				string tabInd = Console.ReadLine();
+				Console.Write("Поле таблицы: ");
+				string tabField = Console.ReadLine();
+				Console.Write("Внешняя таблица: ");
+				string fTab = Console.ReadLine();
+				Console.Write("Внешний индекс: ");
+				string fInd = Console.ReadLine();
+
+
+				string path = $@"C:\Users\user\Desktop\0\GeneratedClasses\{className}.txt";
+
+				string codeInnards = $@"using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PP25_Uchet.Classes.DBClasses.Sprav
+{{
+internal class {className} : DBConnectionClass
+{{
+static public DataTable {dtName} = new DataTable();
+static public void Get{methodName}()
+{{
+	try
+	{{
+		myCommand.CommandText = $@""select * from {bdTab}"";
+		{dtName}.Clear();
+		myDataAdapter.Fill({dtName});
+	}}
+	catch
+	{{
+		MessageBox.Show(""Ошибка при получении {fErMsg}"",
+			""Ошибка"",
+			MessageBoxButtons.OK, MessageBoxIcon.Error);
+	}}
+}}
+
+static public bool Add{methodName}(string value)
+{{
+	try
+	{{
+		myCommand.CommandText = $@""select {tabInd} from {bdTab} where {tabField}=value"";
+		object result = myCommand.ExecuteScalar();
+		if (result == null)
+		{{
+			myCommand.CommandText = $@""insert into {bdTab} values(NULL, '{{value}}')"";
+			myCommand.ExecuteNonQuery();
+			return true;
+		}}
+		MessageBox.Show(""Уже есть {sErMsg}"",
+			""Ошибка"",
+			MessageBoxButtons.OK, MessageBoxIcon.Error);
+		return false;
+	}}
+	catch
+	{{
+		MessageBox.Show(""Ошибка при добавлении {tErMsg}"",
+			""Ошибка"",
+			MessageBoxButtons.OK, MessageBoxIcon.Error);
+		return false;
+	}}
+}}
+
+static public bool Update{methodName}(int index, string value)
+{{
+	try
+	{{
+		myCommand.CommandText = $@""select {tabInd} from {bdTab} where {tabField}='{{value}}' and {tabInd}!={{index}}"";
+		object result = myCommand.ExecuteScalar();
+		if (result == null)
+		{{
+			myCommand.CommandText = $@""update {bdTab} set {tabField}='{{value}}' where {tabInd}={{index}}"";
+			myCommand.ExecuteNonQuery();
+			return true;
+		}}
+		MessageBox.Show(""Уже есть {sErMsg}"",
+			""Ошибка"",
+			MessageBoxButtons.OK, MessageBoxIcon.Error);
+		return false;
+	}}
+	catch
+	{{
+		MessageBox.Show(""Ошибка при обновлении {tErMsg}"",
+			""Ошибка"",
+			MessageBoxButtons.OK, MessageBoxIcon.Error);
+		return false;
+	}}
+}}
+
+static public bool Delete{methodName}(int index)
+{{
+	try
+	{{
+		myCommand.CommandText = $@""select {fInd} from {fTab} where {tabInd}"";
+		object result = myCommand.ExecuteScalar();
+		if (result == null)
+		{{
+			myCommand.CommandText = $@""delete from {bdTab} where {tabInd}={{index}}"";
+			myCommand.ExecuteNonQuery();
+			return true;
+		}}
+		MessageBox.Show(""Уже используется {sErMsg}"",
+			""Ошибка"",
+			MessageBoxButtons.OK, MessageBoxIcon.Error);
+		return false;
+	}}
+	catch
+	{{
+		MessageBox.Show(""Ошибка при удалении {tErMsg}"",
+			""Ошибка"",
+			MessageBoxButtons.OK, MessageBoxIcon.Error);
+		return false;
+	}}
+}}
+}}
+}}";
+
+				using (Stream myStream = File.Open(path, FileMode.OpenOrCreate, FileAccess.Write))
+				{
+					using (var sw = new StreamWriter(myStream))
+					{
+						sw.WriteLine(codeInnards);
+						sw.Close();
+					}
+				}
+
+				File.Move(path, Path.ChangeExtension(path, ".cs"));
+
+				Console.WriteLine("Выход 'y'");
+				if (Console.ReadLine() == "y")
+				{
+					exitState = false;
+				}
+			}
+			
+
+		}
+	}
+}
